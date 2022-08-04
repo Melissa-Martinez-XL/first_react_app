c64d554f1e3b3b06d97ad909ddfdc971e0f1d24f
Melissa <melissa.martinez@xmartlabs.com>
2022-08-04 18:08:22 -0300

    if y list rendering

--- src/Card.js
+++ src/Card.js
@@ -1,31 +1,56 @@
 import { string } from "prop-types";
 
-const Card = ({color, status}) => (
+const Card = ({color, status}) => {
 
-    <div style={{ backgroundColor: color }}>
-        <span>Estado: {status}</span>
+    function showText () {
 
-        {status === 'procesar' ? (
-            <p>Tiene una tarea por procesar.</p>
-        ) : status === 'procesando' ? (
-            <>
-                <div>
-                    <p>Tareas:</p>
-                    <ul>
-                        <li>HTML</li>
-                        <li>JavaScript</li>
-                        <li>CSS</li>
-                    </ul>
-                </div>
-                <p>La tarea se esta procesando.</p>
-            </>
-        ) : status === 'finalizado' ?(
-            <p>La tarea ha finalizado</p>
-        ) : (
-            alert('Un error ha ocurrido')
-        )}
-    </div>
-);
+        if (status === 'procesar') {
+
+            return <p>Tiene una tarea por porcesar</p>;
+        }
+
+        if (status === 'procesando') {
+
+            const list = [
+                {title: 'HTML', id: 1},
+                {title: 'CSS', id: 2},
+                {title: 'JavaScript', id: 3},
+                {title: 'Java', id: 4},
+                {title: 'JSX', id: 5}
+            ];
+
+            const listItems = list.map (items =>
+                <li key={items.id}>
+                    {items.title}
+                </li>
+                );
+            
+            return (
+                <>
+                <p>Tareas:</p>
+                <ul>
+                    {listItems}
+                </ul>
+                <p>La tarea esta siendo procesada</p>
+                </>
+            );
+                
+        }
+
+        return <p>La tarea ha finalizado</p>;
+
+    }
+
+    return (
+        <div style={{ backgroundColor: color }}>
+            <span>Estado: {status}</span>
+
+            {showText()}
+       
+        </div>
+
+    );
+}
 
 /*
 class Card extends Component {
