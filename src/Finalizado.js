339ad9ce61af829f6f20ba9153f796f47b0dff8f
Melissa <melissa.martinez@xmartlabs.com>
2022-08-18 14:24:15 -0300

    Montado y desmontado

---
+++ src/Finalizado.js
@@ -0,0 +1,16 @@
+import { useEffect} from "react";
+
+const Finalizado = (() => {
+
+    useEffect(() => {
+        console.log('Componente montado: Finalizado');
+
+        return () => {
+            console.log('Componente desmontado: Finalizado');
+        }
+    });
+
+    return <p>El proceso ha finalizado </p>
+});
+
+export { Finalizado };
---
+++ src/Procesando.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+
+const Procesando = (() => {
+
+    function list() {
+        const list = [
+            {title: 'HTML', id: 1},
+            {title: 'CSS', id: 2},
+            {title: 'JavaScript', id: 3},
+            {title: 'Java', id: 4},
+            {title: 'JSX', id: 5}
+        ];
+
+        const listItems = list.map (items =>
+            <li key={items.id}>
+                {items.title}
+            </li>
+            );
+        
+        return (
+            <>
+            <p>Tareas:</p>
+            <ul>
+                {listItems}
+            </ul>
+            <p>La tarea esta siendo procesada</p>
+            </>
+        );
+    }
+
+    useEffect(() => {
+        console.log('Componente montado: Procesando');
+
+        return () => {
+            console.log('Componente desmontado: Procesando');
+        }
+    });
+
+    return list();
+});
+
+export { Procesando };
